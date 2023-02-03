1a2f3cf1c509186414bbecb780398d437e044d30
river <lxj.riverluo@gmail.com>
2023-02-03 16:22:47 +0800

    refactor(src): rework bull-planet-head-m
    Added:
      - src\assets\bull\bull-planet-head-m.png
      - src\assets\home\dialog-feed-table-m.png
      - src\assets\home\partnets-bg.png
      - src\assets\minting\prize-icon-gray.png
      - src\assets\page-promo\page-metabull\claim-bull.png
      - src\common\css\base.scss
    
    Updated:
      - src\web3\FBXMetaBullOnLineHelp.js

--- src/web3/FBXMetaBullOnLineHelp.js
+++ src/web3/FBXMetaBullOnLineHelp.js
@@ -25,3 +25,45 @@ export default class FBXMetaBullOnLineHelp extends ContractBase {
     result.mAddr_LastReward = this._web3.utils.fromWei(numArr[7] || '', 'ether') //账户预期可获得的最终奖励
     result.mAddr_inviterNum = numArr[8] //账户邀请的账户数量
 
+    result.mAllInviterLength = numArr[9] //总的邀请账户数量
+    let ith = 10
+    result.mAddr_bHelp = []
+    for (let i = 0; i < 10; i++) {
+      result.mAddr_bHelp[i] = numArr[ith] // 账户是否助力第i（从1开始）个
+      ith += 1
+    }
+    result.mAddr_bGetReward = []
+    for (let i = 0; i < 10; i++) {
+      result.mAddr_bGetReward[i] = numArr[ith] // 账户是否已领取助力第i（从1开始）个
+      ith += 1
+    }
+    result.APool = this._web3.utils.fromWei(numArr[ith] || '', 'ether') // A阵营奖池
+    ith += 1
+    result.BPool = this._web3.utils.fromWei(numArr[ith] || '', 'ether') // B阵营奖池
+    ith += 1
+    result.bEnd = numArr[ith] != 0
+    ith += 1
+    result.bHelpNext = numArr[ith] != 0
+    return result
+  }
+  /**
+   * 直接助力
+   * @param {Number} selType 1-A；2-B
+   */
+  async calHelp(selType) {
+    if (!cWebModel.mConnected) return
+    console.log('---calHelp', selType)
+    return this.sendTransaction('calHelp', [selType])
+  }
+  /**
+   * 受邀助力（仅第一次助力可以调用）
+   * @param {Number} selType 1-A；2-B
+   * @param {String} inviterAddr 邀请者地址
+   */
+  async calHelp_AddInviter(selType, inviterAddr) {
+    if (!cWebModel.mConnected) return
+    console.log('---calHelp_AddInviter', selType, inviterAddr)
+    return this.sendTransaction('calHelp_AddInviter', [selType, inviterAddr])
+  }
+  /**
+   * 获得助力奖励
