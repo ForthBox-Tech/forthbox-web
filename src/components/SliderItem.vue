7b15d3b03540d52100c81e71a77def32aaa6dec3
river <lxj.riverluo@gmail.com>
2023-04-21 13:32:32 +0800

    feat(src): add social-facebook module
    Added:
      - src\assets\comp-footer\social-facebook.png
      - src\assets\comp-user-modal\icon-language.png
      - src\assets\home\partnets-14.png
      - src\assets\page-market2\page-carnivals\banner.png
      - src\assets\page-market2\page-carnivals\notice-new.png
      - src\assets\page-minting\notice.png
      - src\components\SliderItem.vue

---
+++ src/components/SliderItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="fbx-slider-item" @click="onClick">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SliderItem',
+  props: {
+    url: String,
+  },
+  methods: {
+    onClick() {
+      const url = this.url
+      if (!url) return
+
+      if (/^https?/.test(url)) {
+        window.open(url)
+      } else {
+        this.$router.push(url)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.fbx-slider-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: 1;
+  &:not(:first-child) {
+    z-index: 0;
+  }
+}
+</style>
+
