70f34eea0ffa6a7de72fa1cface33e2ea54295d1
river <lxj.riverluo@gmail.com>
2023-08-11 18:52:59 +0800

    fix(src): resolve dialog-nft-title issues
    Added:
      - src\assets\home\dialog-nft-title.png
      - src\assets\page-settings\page-game\input-url.png
      - src\assets\page-settings\page-game\input-verify.png
      - src\modules\TipsDialog.vue
    
    Updated:
      - src\views\Home\modules\SocialFi.vue

---
+++ src/modules/TipsDialog.vue
@@ -0,0 +1,97 @@
+<template>
+  <Dialog class="dialog-tips" :visible="visible" @hide="onHide">
+    <main class="tips-main">
+      <header class="tips-header">
+        <h1 class="tips-title">
+          <slot name="title">Tips</slot>
+        </h1>
+        <a class="tips-close" @click="onHide"></a>
+      </header>
+      <section class="tips-content">
+        <slot name="content"></slot>
+      </section>
+    </main>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from '@/components/Dialog.vue'
+
+export default {
+  name: 'TipsDialog',
+  components: {
+    Dialog,
+  },
+  props: ['visible'],
+  methods: {
+    onHide() {
+      this.$emit('hide')
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.dialog-tips {
+  font-weight: normal;
+  color: #fff;
+  .tips-main {
+    width: 30rem;
+    background-color: #353644;
+    border: 1px solid #444478;
+    border-radius: 1.2rem;
+    @media (max-width: 768.89px) {
+      width: 15rem;
+      border-radius: 0.6rem;
+    }
+  }
+  .tips-header {
+    position: relative;
+    padding: 0 1.5rem;
+    @media (max-width: 768.89px) {
+      padding: 0 1rem;
+    }
+    .tips-title {
+      padding-top: 0.5rem;
+      line-height: 2.9rem;
+      border-bottom: 1px solid rgba(255, 255, 255, 0.42);
+      text-align: left;
+      font-size: 0.9rem;
+      @media (max-width: 768.89px) {
+        padding-top: 0.3rem;
+        line-height: 2rem;
+        font-size: 0.75rem;
+      }
+    }
+    .tips-close {
+      position: absolute;
+      top: 0.5rem;
+      right: 0.5rem;
+      display: inline-block;
+      padding: 0.8rem;
+      width: 1rem;
+      height: 1rem;
+      background: url('~@/assets/common/close.png') center center no-repeat;
+      background-size: 1rem;
+      cursor: pointer;
+      @media (max-width: 768.89px) {
+        top: 0;
+        right: 0;
+        padding: 0.6rem;
+        background-size: 0.8rem;
+      }
+    }
+  }
+  .tips-content {
+    padding: 1rem 1.5rem 1.8rem;
+    line-height: 2;
+    text-align: left;
+    font-size: 0.8rem;
+    @media (max-width: 768.89px) {
+      padding: 0.6rem 1.2rem 1.5rem;
+      font-size: 0.6rem;
+    }
+  }
+}
+</style>
+
--- src/views/Home/modules/SocialFi.vue
+++ src/views/Home/modules/SocialFi.vue
@@ -47,3 +47,53 @@ export default {
       img {
         width: 100%;
       }
+    }
+    .img-bg {
+      position: absolute;
+      top: -1.5rem;
+      left: 50%;
+      width: 14rem;
+      height: 14rem;
+      margin-left: -7rem;
+      opacity: 0.08;
+    }
+    @media (max-width: 768.89px) {
+      margin-top: 2rem;
+      &:after {
+        background-color: #121536;
+        padding-bottom: 2rem;
+        clip-path: polygon(0 0, 0 96%, 50% 100%, 100% 96%, 100% 0, 50% 4%);
+      }
+      .text {
+        padding: 0.5rem 0 1rem;
+        line-height: 1.2rem;
+        text-align: left;
+        font-size: 0.7rem;
+        color: #c6c7e3;
+      }
+    }
+  }
+
+  .forthbox-society {
+    .header,
+    .main .text,
+    .main .banner {
+      opacity: 0;
+    }
+    &.main-animation {
+      .header {
+        animation: blurFadeIn 0.5s ease-in forwards;
+      }
+      .main .text {
+        animation: blurFadeIn 0.8s ease-in forwards;
+        animation-delay: 0.3s;
+      }
+      .main .banner {
+        animation: blurFadeIn 0.8s ease-in forwards;
+        animation-delay: 1s;
+      }
+    }
+  }
+}
+</style>
+
