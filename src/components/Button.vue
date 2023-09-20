489bdfb1a6c59d8d877286fb895b8b456746e656
river <lxj.riverluo@gmail.com>
2023-09-20 15:12:14 +0800

    feat(src): expand favicon implementation
    Added:
      - public\favicon.ico
      - src\common\js\emitter.js
      - src\locales\ru.json
    
    Updated:
      - src\components\Button.vue
      - src\web3\ERC20NFTDeFi.abi.js

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -55,3 +55,48 @@ export default {
     onClick() {
       if (typeof this.handler !== 'function') return
 
+      if (this.iLoading || this.disabled) return
+      this.loader && (this.iLoading = true)
+
+      Promise.resolve(this.handler())
+        .then(() => {
+          this.loader && (this.iLoading = false)
+        })
+        .catch((err) => {
+          console.error(err)
+          this.loader && (this.iLoading = false)
+        })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.fbx-btn {
+  display: inline-block;
+  padding: 0 1.1rem;
+  line-height: 2.2rem;
+  border-radius: 1.1rem;
+  text-align: center;
+  text-decoration: none;
+  font-size: 0.8rem;
+  color: #fff;
+  cursor: pointer;
+
+  &:not(.disabled):hover {
+    opacity: 0.92;
+  }
+  &:not(.disabled):active {
+    opacity: 0.85;
+  }
+  &.btn-color {
+    background: linear-gradient(90deg, #0078ff, #ae00ff);
+  }
+  &.disabled {
+    background: #999 !important;
+    border-color: #999 !important;
+    cursor: not-allowed;
+  }
+}
+</style>
+
--- src/web3/ERC20NFTDeFi.abi.js
+++ src/web3/ERC20NFTDeFi.abi.js
@@ -166,3 +166,171 @@ export default [
         internalType: 'address',
         name: 'WhiteAddress',
         type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getWhiteAccountNum',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'isWhiteContract',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'lastUpdateTime',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'maxStakeNumPerAdress',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tStakePrice',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tTotalNum',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tMaxStakeNumPerAdress',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bool',
+        name: 'tStart',
+        type: 'bool',
+      },
+    ],
+    name: 'notifyRewardAmount',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bytes',
+        name: '',
+        type: 'bytes',
+      },
+    ],
+    name: 'onERC721Received',
+    outputs: [
+      {
+        internalType: 'bytes4',
+        name: '',
+        type: 'bytes4',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'removeWhiteAccount',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'rewardsDuration',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
