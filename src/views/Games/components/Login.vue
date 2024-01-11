4bcec05c347a2b3320ac99ee52f266289a7409c4
ysuo <943711797@qq.com>
2024-01-11 15:26:25 +0800

    refactor(src): reorganize btn-add
    Added:
      - src\assets\page-market2\comp-number-input\btn-add.png
      - src\views\Settings\Game.vue
    
    Updated:
      - src\views\Games\components\Login.vue

--- src/views/Games/components/Login.vue
+++ src/views/Games/components/Login.vue
@@ -61,3 +61,72 @@ export default {
 <style lang="scss" scoped>
 @import '@/common/css/variable.scss';
 
+.dialog-login {
+  $color-blue: #394688;
+
+  .main {
+    position: relative;
+    padding: 0 2.5em 2em;
+    width: 20em;
+    font-size: 1rem;
+    color: $color-white;
+    background-color: rgba($color-blue, 0.7);
+    border: 0.05rem solid rgba($color-white, 0.5);
+    border-radius: 0.8em;
+    overflow: hidden;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      background-color: rgba($color-blue, 1);
+      filter: blur(80px);
+    }
+    @media (max-width: 768.89px) {
+      padding: 0 1.8em 0.8em;
+      width: 16em;
+      font-size: 0.8rem;
+    }
+  }
+  .title {
+    margin-top: 0.3em;
+    line-height: 2.7;
+    font-size: 1.3em;
+  }
+  .item {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    margin: 1.2em 0;
+    padding: 0 1em;
+    line-height: 2.6;
+    font-size: 1.1em;
+    text-align: left;
+    background-color: $color-blue;
+    border-radius: 0.5em;
+    cursor: pointer;
+    .icon {
+      height: 1.2em;
+    }
+    .text {
+      flex: 1;
+      margin: 0 1.2em;
+    }
+    &::after {
+      content: '';
+      display: inline-block;
+      width: 1.2em;
+      height: 0.9em;
+      background: url('~@/assets/common/icon-arrow.png') center center / contain no-repeat;
+      filter: brightness(1.5);
+    }
+    &:hover {
+      background-color: lighten($color-blue, 18);
+    }
+  }
+}
+</style>
+
---
+++ src/views/Settings/Game.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="settings-game">
+    <ProfileSection></ProfileSection>
+    <TabsFrame class="main main-wrap2" :tabs="tabs" @tab-change="onTabChange">
+      <template #AccountSetting>
+        <NavFrame
+          :navs="list"
+          @nav-change="onNavChange"
+          :aside="aside"
+          @aside-trigger="onAsideTrigger"
+        >
+          <template #[game.name]><GameAccounts :game="game"></GameAccounts></template>
+          <template #GameApply><GameApply></GameApply></template>
+        </NavFrame>
+      </template>
+      <template #ManageSetting>
+        <NavFrame
+          :navs="list"
+          @nav-change="onNavChange"
+          :aside="aside"
+          @aside-trigger="onAsideTrigger"
+        >
+          <template #[game.name]><GameManage :game="game"></GameManage></template>
+          <template #GameApply><GameApply></GameApply></template>
+        </NavFrame>
+      </template>
+    </TabsFrame>
+  </div>
+</template>
+
+<script>
+import ProfileSection from '@/modules/ProfileSection.vue'
+import TabsFrame from '@/layouts/TabsFrame.vue'
+import NavFrame from './components/NavFrame.vue'
+import GameAccounts from './game-modules/GameAccounts.vue'
+import GameApply from './game-modules/GameApply.vue'
+import GameManage from './game-modules/GameManage.vue'
+import initTriggerMixin from '@/mixins/init-trigger'
+
+export default {
+  components: {
+    ProfileSection,
+    TabsFrame,
+    NavFrame,
+    GameAccounts,
+    GameApply,
+    GameManage,
+  },
+  data() {
+    return {
+      pageNo: 1,
+      pageSize: 20,
+
+      total: 0,
+      list: [],
+
+      aside: {
+        // icon: require('@/assets/page-settings/page-account/nav-game-add.png'),
+        // text: '添加游戏',
+        // name: 'GameApply',
+      },
+
