c0e8331c8af2777623b921660d8d96418b86d6d1
Jamesfish01 <fish@forthbox.io>
2024-04-09 15:21:54 +0800

    refactor(src): reorganize .env
    Added:
      - .env.development
    
    Updated:
      - src\modules\Footer.vue
      - src\views\Market2\common\helper.js

--- src/modules/Footer.vue
+++ src/modules/Footer.vue
@@ -261,3 +261,289 @@ export default {
 <style lang="scss" scoped>
 @import '@/common/css/variable.scss';
 
+.comp-footer {
+  line-height: 1.5;
+  text-align: left;
+  font-size: 0.8rem;
+  color: $color-secondary;
+  background-color: #f8f8ff;
+  @media (max-width: 768.89px) {
+    font-size: 0.6rem;
+  }
+  .main {
+    margin: 0 auto;
+    max-width: 72rem;
+    padding: 2.8rem 2rem 0;
+    @media (max-width: 768.89px) {
+      padding: 1.6rem 1rem 0;
+    }
+  }
+  .layout {
+    display: flex;
+    @media (max-width: 768.89px) {
+      display: block;
+    }
+    .col {
+      flex: 1;
+    }
+  }
+  .infos {
+    .logo {
+      display: block;
+      width: 9.5rem;
+      @media (max-width: 768.89px) {
+        margin: 0 auto;
+      }
+    }
+    .support {
+      margin: 2rem 0 4rem;
+      max-width: 22rem;
+      display: flex;
+      align-items: center;
+      background-color: rgba($color-purple, 0.1);
+      border-radius: 2rem;
+      overflow: hidden;
+      @media (max-width: 768.89px) {
+        margin: 1.2rem 0 1.8rem;
+        max-width: none;
+        font-size: 1.1em;
+      }
+      .label {
+        padding: 0 0.8em 0 1.6em;
+        color: $color-primary;
+      }
+      .input {
+        flex: 1;
+        width: 3rem;
+        padding: 0 0.8em;
+        line-height: 2.5;
+        font-size: inherit;
+        color: inherit;
+        background: transparent;
+        border: 0 none;
+        outline: none;
+      }
+      .btn-copy {
+        @include btn-fill-pure($color-purple);
+        box-sizing: border-box;
+        width: 5rem;
+        line-height: 2rem;
+        border-radius: 2rem;
+        @media (max-width: 768.89px) {
+          width: 4.2rem;
+          border-radius: 2rem;
+          font-size: 1.2em;
+        }
+      }
+    }
+    .text {
+      max-width: 25rem;
+      margin: 1em 0;
+      font-size: 1.125em;
+      color: $color-primary;
+      @media (max-width: 768.89px) {
+        max-width: none;
+      }
+      .link {
+        margin: 0 0 0 0.3em;
+        color: $color-blue;
+        cursor: pointer;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+  .more {
+    @media (max-width: 768.89px) {
+      margin-top: 1rem;
+    }
+    .links {
+      display: flex;
+      justify-content: flex-end;
+      @media (max-width: 768.89px) {
+        justify-content: space-between;
+      }
+      .part {
+        margin-left: 4rem;
+        line-height: 1.7;
+        font-weight: 500;
+        @media (max-width: 768.89px) {
+          margin-left: 0;
+        }
+      }
+      .title {
+        margin-bottom: 0.2em;
+        font-size: 1.25em;
+        opacity: 0.85;
+      }
+      .link {
+        display: block;
+        color: $color-blue;
+        white-space: nowrap;
+        cursor: pointer;
+        opacity: 0.65;
+        &::before {
+          content: '';
+          margin-right: 0.5em;
+          display: inline-block;
+          width: 0.3em;
+          height: 0.3em;
+          border-top: 0.05rem solid $color-blue;
+          border-right: 0.05rem solid $color-blue;
+          transform: rotate(45deg);
+          vertical-align: 0.15em;
+        }
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+    .social {
+      margin-top: 2rem;
+      text-align: right;
+      @media (max-width: 768.89px) {
+        margin-top: 1rem;
+        text-align: left;
+      }
+      .title {
+        margin-bottom: 1.12em;
+        font-size: 0.9em;
+        opacity: 0.7;
+      }
+      .item {
+        margin-left: 1rem;
+        display: inline-block;
+        vertical-align: middle;
+        cursor: pointer;
+        @media (max-width: 768.89px) {
+          margin: 0 0.5rem 0 0;
+        }
+      }
+      .icon {
+        display: block;
+        width: 1.5rem;
+        @media (max-width: 768.89px) {
+          width: 1.25rem;
+        }
+      }
+    }
+  }
+  .aside {
+    margin-top: 1rem;
+    border-top: 0.05rem solid $color-border;
+    padding: 1.45em 0 2em;
+    display: block;
+    text-align: center;
+    font-size: 0.7rem;
+    @media (max-width: 768.89px) {
+      font-size: 0.6rem;
+    }
+  }
+}
+.dialog-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .footer-main {
+    padding: 1.2rem 2.2rem 1.8rem;
+    background-color: $color-white;
+    font-size: 0.8rem;
+    color: $color-primary;
+    border: 0.05rem solid $color-border;
+    border-radius: 0.8rem;
+    @media (max-width: 768.89px) {
+      padding: 0.7rem 1rem 1.2rem;
+      border-radius: 0.5rem;
+      font-size: 0.65rem;
+    }
+  }
+  .close {
+    position: absolute;
+    top: 0.8rem;
+    right: 0.8rem;
+    display: block;
+    width: 1.6rem;
+    height: 1.6rem;
+    background: url('~@/assets/common/close.png') center center / 60% no-repeat;
+    cursor: pointer;
+    @media (max-width: 768.89px) {
+      top: 0.4rem;
+      right: 0.4rem;
+      width: 1.2rem;
+      height: 1.2rem;
+    }
+  }
+  .title {
+    margin-bottom: 1.5rem;
+    text-align: center;
+    font-size: 1.5em;
+    @media (max-width: 768.89px) {
+      margin: 0 0 0.8rem;
+    }
+  }
+}
+.dialog-email {
+  .line {
+    margin-bottom: 0.65rem;
+    @media (max-width: 768.89px) {
+      margin-bottom: 0.5rem;
+    }
+    .input {
+      font-size: 1.2em;
+      background: transparent;
+      border: 0 none;
+      outline: none;
+    }
+    .btn-copy {
+      @include btn-fill-pure($color-purple);
+      padding: 0;
+      margin-left: 5rem;
+      width: 5.15rem;
+      line-height: 1.6rem;
+      font-size: inherit;
+      border-radius: 1.6rem;
+      @media (max-width: 768.89px) {
+        width: 3rem;
+        margin-left: 2rem;
+        line-height: 1.2rem;
+        border-radius: 1.6rem;
+      }
+    }
+  }
+}
+.dialog-contract {
+  .list {
+    margin: -1.2rem -2rem 0;
+    padding: 0 2rem;
+    max-height: 18.5rem;
+    overflow-y: auto;
+    @media (max-width: 768.89px) {
+      margin: -0.6rem -1rem 0;
+      padding: 0 1rem;
+    }
+  }
+  .table {
+    line-height: 1.4;
+    text-align: left;
+    th {
+      padding: 0.2rem 0.3rem;
+    }
+    td {
+      padding: 0.2rem 0.3rem;
+      @media (max-width: 768.89px) {
+        max-width: 6.5rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+    a {
+      color: $color-blue;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+</style>
+
--- src/views/Market2/common/helper.js
+++ src/views/Market2/common/helper.js
@@ -22,3 +22,37 @@ export const getFbxPrice = (() => {
   }
 })()
 
+/**
+ * 创建计时器（用户记录数据展示时间）
+ * @param {Function} callback 回调函数
+ * @returns {{ stop: Function }} timer 计时器控制对象，可调用stop关闭计时器
+ */
+export function createTimer(callback) {
+  function format(_seconds) {
+    if (_seconds < 60) {
+      // 小于1分钟，展示：xx秒（s）
+      return `${_seconds}s`
+    } else if (_seconds < 3600) {
+      // 大于等于1分钟，小于1小时，展示：xx分（m）
+      return `${Math.floor(_seconds / 60)}m`
+    } else if (_seconds < 86400) {
+      // 大于等于1小时，小于1天，展示：xx时（h）
+      return `${Math.floor(_seconds / 3600)}h`
+    } else {
+      // 大于等于1天，展示：xx天（d）
+      return `${Math.floor(_seconds / 86400)}d`
+    }
+  }
+
+  let seconds = 0
+  let timer = setInterval(() => {
+    seconds += 1
+    typeof callback === 'function' && callback(seconds, format(seconds))
+  }, 1000)
+
+  return {
+    stop() {
+      clearInterval(timer)
+    },
+  }
+}
