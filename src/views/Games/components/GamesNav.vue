cc6f3574872ac26c7c4486ab6350693439a86f88
Jamesfish01 <fish@forthbox.io>
2024-04-17 15:02:45 +0800

    fix(src): resolve banner-5 issues
    Added:
      - src\assets\home\banner-5.jpg
    
    Updated:
      - src\common\css\base.scss
      - src\views\Games\components\GamesNav.vue

--- src/views/Games/components/GamesNav.vue
+++ src/views/Games/components/GamesNav.vue
@@ -140,3 +140,107 @@ export default {
       this._toggle('DrawRecordDialog')
     },
 
+    async _onWalletChange() {
+      if (this.$root.token) return
+
+      const isExist = await this._isExistUser()
+      if (isExist) {
+        await this.$root.sign()
+        await this.$root.getUserInfo()
+        emitter.emit('auth-change')
+      } else {
+        this._toggle('WalletSignDialog')
+      }
+    },
+    async _isExistUser() {
+      const url = `${process.env.VUE_APP_API_FBOX2}/web/users/is_exist`
+      const params = {
+        type: 'userAddr',
+        value: this.$root.userWallet,
+      }
+
+      const res = await this.$axios.get(url, { params })
+      if (res.code != 200) {
+        console.warn(res.msg)
+        return
+      }
+
+      return !!res.data?.isExisted
+    },
+  },
+  mounted() {},
+  created() {
+    emitter.on('wallet-connect', this.onProfile)
+    emitter.on('wallet-change', this._onWalletChange)
+  },
+  unmounted() {
+    emitter.off('wallet-connect', this.onProfile)
+    emitter.off('wallet-change', this._onWalletChange)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/common/css/variable.scss';
+
+.comp-games-nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  color: $color-primary;
+  background-color: $color-white;
+  box-shadow: 0 0 0.8rem 0 rgba(91, 92, 97, 0.1);
+  -webkit-tap-highlight-color: transparent;
+  &.transparent {
+    color: $color-white;
+    background-color: transparent;
+    box-shadow: none;
+  }
+  .main-wrap2 {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: var(--nav2-height);
+    margin-top: 0;
+    margin-bottom: 0;
+    color: inherit;
+  }
+  .nav {
+    display: flex;
+    align-items: center;
+    font-size: 0.8rem;
+    @media (max-width: 768.89px) {
+      font-size: 0.65rem;
+    }
+    .link {
+      display: block;
+      margin-right: 1.5rem;
+      font-weight: 500;
+      color: inherit;
+      cursor: pointer;
+      @media (max-width: 768.89px) {
+        margin-right: 0.5rem;
+      }
+      &.link-home {
+        margin-right: 2.5rem;
+        @media (max-width: 768.89px) {
+          margin-right: 0.5rem;
+        }
+      }
+      &:hover,
+      &.active {
+        color: $color-blue;
+      }
+    }
+    .logo {
+      display: block;
+      width: 6.9rem;
+      @media (max-width: 768.89px) {
+        width: 5.4rem;
+      }
+    }
+  }
+
