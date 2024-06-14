c1643ced1f8ae40bfc9e36dffc218cf324f1fc26
ysuo <943711797@qq.com>
2024-06-14 14:46:30 +0800

    test(src): add regression test for connectdialog
    Updated:
      - src\modules\ConnectDialog.vue
      - src\views\DeFi\defi.common.scss
      - src\web3\ForthBoxMarketSwap.abi.js

--- src/modules/ConnectDialog.vue
+++ src/modules/ConnectDialog.vue
@@ -71,3 +71,103 @@ export default {
 <style lang="scss" scoped>
 @import '@/common/css/variable.scss';
 
+.connect-dialog {
+  .connect-main {
+    box-sizing: border-box;
+    width: 26rem;
+    font-size: 1rem;
+    text-align: left;
+    background-color: $color-white;
+    border-radius: 0.8em;
+    @media (max-width: 768.89px) {
+      width: 15rem;
+      font-size: 0.8rem;
+    }
+    &::before,
+    &::after {
+      content: '';
+      display: table;
+      clear: both;
+    }
+  }
+  .header {
+    display: flex;
+    align-items: center;
+    line-height: 3.5em;
+    .title {
+      flex: 1;
+      margin: 0 0 0 2.5em;
+      font-size: inherit;
+      font-weight: 500;
+      color: $color-primary;
+      text-align: center;
+    }
+    .close {
+      display: block;
+      margin-right: 0.5em;
+      width: 2em;
+      height: 2em;
+      background: url('~@/assets/common/close.png') center center / 0.8em no-repeat;
+      cursor: pointer;
+      &:hover {
+        filter: brightness(0.8);
+      }
+    }
+  }
+  .content {
+    padding: 0 2em 2em;
+    font-size: 0.8em;
+    color: #6b6b6e;
+    .list {
+      margin: 0 0 0.8em;
+      border: 0.05rem solid lighten($color-border, 3);
+      border-radius: 0.5em;
+      overflow: hidden;
+    }
+    .item {
+      display: flex;
+      align-items: center;
+      padding: 0 1.2em;
+      line-height: 3.75em;
+      text-align: left;
+      font-weight: 500;
+      border-bottom: 0.05rem solid lighten($color-border, 6);
+      cursor: pointer;
+      &:last-child {
+        border-bottom: 0 none;
+      }
+      &:hover {
+        background-color: darken($color-white, 2);
+      }
+      .icon {
+        margin-right: 0.8em;
+        width: 1.8em;
+        height: 1.8em;
+      }
+      .text {
+        flex: 1;
+      }
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 1.4em;
+        height: 1em;
+        background: url('~@/assets/common/icon-arrow.png') center center / contain no-repeat;
+      }
+    }
+    .aside {
+      padding: 0.2em 0;
+      line-height: 1.4;
+      font-size: 0.85em;
+      .link {
+        color: $color-blue;
+        cursor: pointer;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+}
+</style>
+
--- src/web3/ForthBoxMarketSwap.abi.js
+++ src/web3/ForthBoxMarketSwap.abi.js
@@ -326,3 +326,331 @@ export default [
       },
       {
         internalType: 'address',
+        name: 'erc20Addr',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'price',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'key',
+        type: 'uint256',
+      },
+    ],
+    name: 'planSell',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'contractTypeInt',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'contractAddr',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'token_ids',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'address',
+        name: 'erc20Addr',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'prices',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'key',
+        type: 'uint256',
+      },
+    ],
+    name: 'planSells',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'removeWhiteAccount',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'token_id_sell',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'key',
+        type: 'uint256',
+      },
+    ],
+    name: 'sell',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256[]',
+        name: 'ids',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'token_id_sells',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'key',
+        type: 'uint256',
+      },
+    ],
+    name: 'sells',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'contractAddr',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'feeRate',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'feeAddress',
+        type: 'address',
+      },
+    ],
+    name: 'setProjectFee',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+    ],
+    name: 'checkID',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256[]',
+        name: 'ids',
+        type: 'uint256[]',
+      },
+    ],
+    name: 'checkIDs',
+    outputs: [
+      {
+        internalType: 'bool[]',
+        name: '',
+        type: 'bool[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'contractAddr',
+        type: 'address',
+      },
+    ],
+    name: 'getProjectFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'feeRate',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'sumProjectFee',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'sumFundFee',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'feeAddress',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'isWhiteContract',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'mFeeRate',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'mFundAdress',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'mSumAllFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'mSumFundFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
